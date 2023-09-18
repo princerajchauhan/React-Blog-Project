45772bf61d8d47c4fc13dd2f2dbbd4056e854776
Prince <princeraj.chauhan@outlook.com>
2023-09-18 13:09:33 +0530

    Routing component

--- src/Routing/Routing.js
+++ src/Routing/Routing.js
@@ -11,16 +11,10 @@ import Hollywood from '../Components/Hollywood'
 import Food from '../Components/Food'
 import CategoryDetail from '../Components/CategoryDetail'
 import Footer from '../Components/Footer'
-<<<<<<< HEAD
-=======
-import { Context } from '../Data'
->>>>>>> f8a4aabfcf6644fce44a4331f78b43ee25c75894
-
 
 const Routing = () => {
     return (
         <>
-<<<<<<< HEAD
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/bollywood" element={<Bollywood />} />
@@ -31,20 +25,6 @@ const Routing = () => {
                 <Route path="/details/:id" element={<CategoryDetail />} />
             </Routes>
             <Footer />
-=======
-            <Context>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/bollywood" element={<Bollywood />} />
-                    <Route path="/technology" element={< Technology />} />
-                    <Route path="/hollywood" element={<Hollywood />} />
-                    <Route path="/fitness" element={<Fitenss />} />
-                    <Route path="/food" element={<Food />} />
-                    <Route path="/details/:id" element={<CategoryDetail />} />
-                </Routes>
-                <Footer />
-            </Context>
->>>>>>> f8a4aabfcf6644fce44a4331f78b43ee25c75894
         </>
     )
 }
